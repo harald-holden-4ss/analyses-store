a3915b540e1c4a6db55780e2bdb276cb6b7c0add
harald-holden-4ss <harald.holden@4subsea.com>
2023-06-14 09:12:31 +0200

    commented out routes

--- app/fast_api_app.py
+++ app/fast_api_app.py
@@ -10,15 +10,15 @@ from .services.database_service import database_service
 def get_app():
     db_serv = database_service()
     app = FastAPI()
-    api = APIRouter(prefix="/api")
-    vessels_routes = get_router_one_collection(db_serv, "vessels", vessel)
-    analyses_base_routes = get_router_one_collection(db_serv, "analyses", analysis_result)
-    analyses_advanced_routes = get_advanced_analyses_results_routes(db_serv,
-        "analysesresults")
-    api.include_router(vessels_routes)
-    api.include_router(analyses_base_routes)
-    api.include_router(analyses_advanced_routes)
-    app.include_router(api)
+    # api = APIRouter(prefix="/api")
+    # vessels_routes = get_router_one_collection(db_serv, "vessels", vessel)
+    # analyses_base_routes = get_router_one_collection(db_serv, "analyses", analysis_result)
+    # analyses_advanced_routes = get_advanced_analyses_results_routes(db_serv,
+    #     "analysesresults")
+    # api.include_router(vessels_routes)
+    # api.include_router(analyses_base_routes)
+    # api.include_router(analyses_advanced_routes)
+    # app.include_router(api)
 
     @app.get("/ping")
     def pingpong():
